d12c7f1ba2fb1b24a56da1bd4caa8acbe2fe1cdb
Özcan Oğuz <ozcan@oyd.org.tr>
2026-02-18 01:08:28 +0300

    build(release): Bump version

--- version.php
+++ version.php
@@ -4,10 +4,10 @@
 
 
 <?php
-defined('MOODLE_INTERNAL') || die();
-$plugin->component = 'profilefield_tckn';
-$plugin->version   = 2026021602;
-$plugin->release   = '1.0.1';
-$plugin->maturity  = MATURITY_STABLE;
-// Moodle 4.4 core version.
+    defined('MOODLE_INTERNAL') || die();
+    $plugin->component = 'profilefield_tckn';
+    $plugin->version   = 2026021603;
+    $plugin->release   = '1.0.2';
+    $plugin->maturity  = MATURITY_STABLE;
+    // Moodle 4.4 core version.
 $plugin->requires = 2024042200;
