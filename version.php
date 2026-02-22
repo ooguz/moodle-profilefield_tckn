5b29a6a985425e235a772d1fb11bedb789e3d949
Özcan Oğuz <ozcan@oyd.org.tr>
2026-02-22 13:19:21 +0300

    build: Bump version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'profilefield_tckn';
-$plugin->version   = 2026021604;
-$plugin->release   = '1.0.3';
+$plugin->version   = 2026021605;
+$plugin->release   = '1.0.4';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2024042200;
